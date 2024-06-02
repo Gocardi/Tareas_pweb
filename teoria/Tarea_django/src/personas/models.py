cf0ba1a6aada39a665581c9afad914a8596a05b5
L0rD1ego <diegoalejandrocarbajal@gmail.com>
2024-06-01 22:40:07 -0500

    Paso 5: Definir modelos en personas/models.py

--- teoria/Tarea_django/src/personas/models.py
+++ teoria/Tarea_django/src/personas/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+class Persona(models.Model):
+    nombre = models.CharField(max_length=100)
+    email = models.EmailField()
+    telefono = models.Charfield(max_length=9)
 # Create your models here.
