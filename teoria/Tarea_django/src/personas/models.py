33fb2ddccf99221394f334ecdacec2c54137cef0
L0rD1ego <diegoalejandrocarbajal@gmail.com>
2024-06-01 22:51:56 -0500

    Paso 6: Crear y aplicar migraciones para los modelos de presonas , tambien se modifico el arvhivo settings.py

--- teoria/Tarea_django/src/personas/models.py
+++ teoria/Tarea_django/src/personas/models.py
@@ -3,5 +3,5 @@ from django.db import models
 class Persona(models.Model):
     nombre = models.CharField(max_length=100)
     email = models.EmailField()
-    telefono = models.Charfield(max_length=9)
+    telefono = models.CharField(max_length=20)
 # Create your models here.
