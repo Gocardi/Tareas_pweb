f9470fb4bedae1fca02dc6708df2da0951fee3fd
L0rD1ego <diegoalejandrocarbajal@gmail.com>
2024-06-08 13:16:10 -0500

    Se modifico el modelo persona con los campos adecuados

--- teoria/Tarea_django/src/personas/models.py
+++ teoria/Tarea_django/src/personas/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 class Persona(models.Model):
     nombre = models.CharField(max_length=100)
-    email = models.EmailField()
-    telefono = models.CharField(max_length=20)
+    edad = models.IntegerField()
+    biografia = models.TextField()
 # Create your models here.
