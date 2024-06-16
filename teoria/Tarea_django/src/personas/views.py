d1e14cb8a3d59f65b37c852114caf87758e3138b
L0rD1ego <diegoalejandrocarbajal@gmail.com>
2024-06-15 21:30:59 -0500

    Configuracion inicial del proyecto y aplicacion 'personas', creacion de vista con contexto y template

--- teoria/Tarea_django/src/personas/views.py
+++ teoria/Tarea_django/src/personas/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
+def index(request):
+    context = {'mensaje': 'Hola, mundo!'}
+    return render(request, 'personas/index.html', context)
 # Create your views here.
