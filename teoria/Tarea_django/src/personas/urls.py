1decd3eec9b50ff6048b37659074f33b07ec736d
L0rD1ego <diegoalejandrocarbajal@gmail.com>
2024-06-23 23:23:15 -0500

    Registrar vistas basadas en clases en personas.urls

--- teoria/Tarea_django/src/personas/urls.py
+++ teoria/Tarea_django/src/personas/urls.py
@@ -1,7 +1,21 @@
-from django.urls import path 
-from .views import crear_persona
+# personas/urls.py
+from django.urls import path
+from .views import (
+    PersonaListView,
+    PersonaDetailView,
+    PersonaCreateView,
+    PersonaUpdateView,
+    PersonaDeleteView,
+    crear_persona
+)
 
 app_name = 'personas'
+
 urlpatterns = [
+    path('', PersonaListView.as_view(), name='lista'),
+    path('<int:pk>/', PersonaDetailView.as_view(), name='detalle'),
+    path('crear/', PersonaCreateView.as_view(), name='crear'),
+    path('editar/<int:pk>/', PersonaUpdateView.as_view(), name='editar'),
+    path('borrar/<int:pk>/', PersonaDeleteView.as_view(), name='borrar'),
     path('crear-persona/', crear_persona, name='crear_persona'),
 ]
