13ee7b0d5dce7d0e1b3c212d387f53fe2b819b1d
L0rD1ego <diegoalejandrocarbajal@gmail.com>
2024-06-23 22:56:35 -0500

    Crear la vista para manejar el formulario PersonaForm

--- teoria/Tarea_django/src/personas/views.py
+++ teoria/Tarea_django/src/personas/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import Persona
+
+def crear_persona(request):
+    if request.method == 'POST':
+        form = PresonaForm(request.POST)
+        if form.is_valid():
+            #aqui se procesan los datos limpios
+            return redirect('personas:lista')
+    else:
+        form = PersonaForm()
+    return render(request, 'personas/persona_form.html', {'form':form})
+
+
 def persona_detail(request, persona_id):
     persona = Persona.objects.get(id=persona_id)
     return render(request, 'personas/detail.html', {'persona': persona})
+
+
 def index(request):
     context = {'mensaje': 'Hola, mundo!'}
     return render(request, 'personas/index.html', context)
